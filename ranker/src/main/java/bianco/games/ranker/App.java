df91b6ca43b2d8a68dfc892b43ececda25a543c6
Bob Bianchi <rbianx@gmail.com>
2024-05-16 22:49:12 -0400

    finished A/B ranking logic

--- ranker/src/main/java/bianco/games/ranker/App.java
+++ ranker/src/main/java/bianco/games/ranker/App.java
@@ -4,10 +4,28 @@ import bianco.games.ranker.models.Role;
 import bianco.games.ranker.models.User;
 import bianco.games.ranker.models.Board;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class App {
+    static ArrayList<String> QB_MASTER = new ArrayList<>(Arrays.asList(
+            "Patrick Mahomes",
+            "Josh Allen",
+            "Justin Herbert"
+//            "Lamar Jackson",
+//            "Joe Burrow",
+//            "Jared Goff",
+//            "Dak Prescott",
+//            "Trevor Lawrence",
+//            "Tua Tagovailoa",
+//            "Jalen Hurts",
+//            "Brock Purdy",
+//            "CJ Stroud",
+//            "Jordan Love"
+    ));
+
     public static void main(String[] args) {
         User currentUser = startUp();
         menu(currentUser);
@@ -37,7 +55,7 @@ public class App {
             switch (input) {
                 case "1" ->
                     //do create list
-                        System.out.println("Create list Coming soon");
+                        doCreate();
                 case "2" ->
                     //do read list
                         System.out.println("Read list coming soon");
@@ -77,5 +95,69 @@ public class App {
         } else {
             System.out.println("no data");
         }
+    }
+
+    static void doCreate() {
+        //initilize variables and copy QB_MASTER
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> submission = new ArrayList<>();
+        int submissionIndex = 0;
+        ArrayList<String> qbList = new ArrayList<>(QB_MASTER);
+        int masterIndex = qbList.size() - 1;
+
+        //add last qb in list to submission and remove it from qbList,
+        submission.add(qbList.get(masterIndex));
+        qbList.remove(masterIndex);
+        masterIndex--;
+
+        //A/B logic
+        while (masterIndex > -1) {
+            System.out.printf("%nIs %s better than %s? ", qbList.get(masterIndex), submission.get(submissionIndex));
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("n") || input.equalsIgnoreCase("no")) {
+                submission.add(submissionIndex + 1, qbList.get(masterIndex));
+                if (masterIndex >0) {
+                    masterIndex--;
+                } else {
+                    break;
+                }
+                printMasterIndex(qbList, masterIndex);
+                submissionIndex = submission.size() - 1;
+                printSubmissionIndex(submission, submissionIndex);
+                printSubmission(submission);
+            } else if (input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes")) {
+                if (submissionIndex == 0) {
+                    submission.add(0, qbList.get(masterIndex));
+                    printSubmission(submission);
+                    if (masterIndex >0) {
+                        masterIndex--;
+                    } else {
+                        break;
+                    }
+                    printMasterIndex(qbList, masterIndex);
+                    submissionIndex = submission.size() - 1;
+                    printSubmissionIndex(submission, submissionIndex);
+                } else {
+                    submissionIndex--;
+                    printSubmissionIndex(submission, submissionIndex);
+                }
+            } else {
+                System.out.println("invalid input");
+            }
+        }
+        System.out.println("#####################");
+        System.out.printf("Final submission: %s%n", submission.toString());
+        System.out.println("#####################");
+    }
+
+    static void printSubmission(ArrayList<String> submission) {
+        System.out.printf("Current List: %s%n", submission.toString());
+    }
+
+    static void printMasterIndex(ArrayList<String> list, int index) {
+        System.out.printf("Master Index : %s(%s)  ", index, list.get(index) );
+    }
+    static void printSubmissionIndex(ArrayList<String> list, int index) {
+        System.out.printf("Submission Index : %s(%s)  ", index, list.get(index) );
     }
 }
